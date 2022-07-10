619d492ae814c0f52c9e8c0a0188a8f2c73b162a
Kaithwar <101472712+Kaithwar@users.noreply.github.com>
2022-07-10 18:24:13 +0530

    Intial Commit

---
+++ script.js
@@ -0,0 +1,120 @@
+console.log("Welcome To Spotify");
+
+// Intialize the variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterplay');
+let mybar = document.getElementById('mybar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    { songName: "Warriyo - Mortals [NCS Realease]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Gal Ban Gayi", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
+    { songName: "Deaf kev - Invincible", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
+    { songName: "Different Heaven & Ehide - My Heart", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
+    { songName: "Janji- Hereos Tonight", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
+    { songName: "Garmi-Street Dancer", filePath: "songs/6.mp3", coverPath: "covers/6.jpg" },
+    { songName: "Coca Cola Tu - Tony Kakkar", filePath: "songs/7.mp3", coverPath: "covers/7.jpg" },
+    { songName: "Ishare Tere- Guru Randhawa", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" },
+    { songName: "Nikle Current - Jassie Gill", filePath: "songs/9.mp3", coverPath: "covers/9.jpg" },
+    { songName: "Vaaste - Dhvani Bhanushali", filePath: "songs/10.mp3", coverPath: "covers/10.jpg" },
+]
+
+songItems.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
+
+// audioElement.play();
+
+// Handle play/pause click
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+
+
+// Listen To Events
+audioElement.addEventListener('timeupdate', () => {
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+    mybar.value = progress;
+})
+
+mybar.addEventListener('change', () => {
+    audioElement.currentTime = mybar.value * audioElement.duration / 100;
+})
+
+const makeAllPlays = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex + 1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+
+    })
+})
+
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex >= 9) {
+        songIndex = 0;
+    }
+    else {
+        console.log(songIndex);
+        songIndex = songIndex + 1;
+    }
+    audioElement.src = `songs/${songIndex + 1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    makeAllPlays();
+    audioElement.play();
+    gif.style.opacity = 1;
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+document.getElementById('previous').addEventListener('click', () => {
+    if (songIndex <= 0) {
+        songIndex = 0;
+    }
+    else {
+        console.log(songIndex);
+        songIndex = songIndex - 1;
+    }
+    audioElement.src = `songs/${songIndex - 1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    makeAllPlays();
+    audioElement.play();
+    gif.style.opacity = 1;
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
